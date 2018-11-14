903296279ef673d2b2a98fd9ab41fcf026b529bc
Anna Fodor <fodor.ak@gmail.com>
2018-11-14 13:23:29 +0100

    vote + edit answer

--- data_manager.py
+++ data_manager.py
@@ -43,9 +43,45 @@ def get_question_details(cursor, id):
 @connection.connection_handler
 def get_answer_details(cursor, id):
     cursor.execute('''
-                    SELECT vote_number, message FROM answer
+                    SELECT vote_number, message, id FROM answer
                     WHERE question_id = %(id)s;
                     ''',
                    {'id': id})
     details = cursor.fetchall()
     return details
+
+
+@connection.connection_handler
+def get_the_answer(cursor, id):
+    cursor.execute('''
+                    SELECT message, id, question_id FROM answer
+                    WHERE id = %(id)s
+                    ''',
+                   {'id': id})
+    details = cursor.fetchone()
+    return details
+
+
+@connection.connection_handler
+def change_vote_number(cursor, vote, id):
+    if vote == "Vote up":
+        vote_num = 1
+    if vote == "Vote down":
+        vote_num = -1
+    cursor.execute('''
+                    UPDATE question
+                    SET vote_number = vote_number + %(vote_num)s
+                    WHERE id = %(id)s;
+                    ''',
+                   {'id': id, 'vote_num': vote_num})
+
+
+@connection.connection_handler
+def get_current_answer_details(cursor, id, new_message):
+    cursor.execute('''
+                    UPDATE answer
+                    SET message = %(new_message)s
+                    WHERE id = %(id)s;
+                    ''',
+                   {'id': id, 'new_message': new_message})
+
--- server.py
+++ server.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
-import data_manager
+from flask import Flask, render_template, request, redirect
 import data_manager
 import connection
 import time
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def route_list():
     questions = data_manager.show_questions_by_order(request.args.get('select_order'))
     return render_template('list.html', questions=questions)
+
 
 @app.route("/ask-question", methods=["GET", "POST"])
 def route_ask_question():
@@ -17,6 +18,7 @@ def route_ask_question():
         return render_template('ask-question.html')
     else:
         return "matéka meleg"
+
 
 @app.route("/question/<id>", methods=["GET", "POST"])
 def route_question(id):
@@ -32,9 +34,18 @@ def route_delete_answer(answer_id, id_):
     pass
 
 
-@app.route("/question/<id>/<vote>")
-def route_question_voting(id, vote):
-    pass
+@app.route("/question/vote_up/<id>", methods=["POST"])
+def route_question_voting_up(id):
+    vote = "Vote up"
+    data_manager.change_vote_number(vote, id)
+    return redirect("/")
+
+
+@app.route("/question/vote_down/<id>", methods=["POST"])
+def route_question_voting_down(id):
+    vote = "Vote down"
+    data_manager.change_vote_number(vote, id)
+    return redirect("/")
 
 
 @app.route("/question/<id>/<vote>/<submission_time>")
@@ -45,6 +56,18 @@ def route_answer_voting(id, vote, submission_time):
 @app.route("/question/<id>/edit", methods=["GET", "POST"])
 def route_edit_question(id):
     pass
+
+
+@app.route("/answer/<answer_id>/edit", methods=["GET", "POST"])
+def route_edit_answer(answer_id):
+    answer_detail = data_manager.get_the_answer(answer_id)
+    if request.method == "GET":
+        return render_template('answer.html', answer_detail=answer_detail)
+    else:
+        new_message = request.form["edit_answer_text"]
+        data_manager.get_current_answer_details(answer_id, new_message)
+        print(answer_detail)
+        return redirect("/question/" + str(answer_detail['question_id']))
 
 
 if __name__ == "__main__":
